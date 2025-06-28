6426857d5bb14ccdaac5eb9ba16d2c4523fac1f9
sytraaak <burdajen@gmail.com>
2025-06-28 09:25:13 +0200

    hw2 review

--- Ukol_2_Jana Barotova.py
+++ Ukol_2_Jana Barotova.py
@@ -1,33 +1,75 @@
 #Ukol 2 pro Programování v Pythonu - Jana Barotová
 
 import requests 
-import json
 
-ico = input("Zadejte IČO subjektu: ")
-url = "https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/" + ico
+def get_info_by_ico(ico: int):
+    url = f"https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/{ico}"
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f"{response.json()["popis"].split("|")[1]}")
+        return None
+    return response.json()
 
-odpoved_ico = requests.get(url)
-data_ico = odpoved_ico.json()         
+def get_info_by_name(name: str):
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/json"
+    }
+    url = "https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/vyhledat"
+    data = {"obchodniJmeno": name}
 
-print("\nObchodní jméno:")
-print(data_ico["obchodniJmeno"])
-print("Sídlo:")
-print(data_ico["sidlo"]["textovaAdresa"])
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code != 200:
+        return None
 
-nazev = input("\nZadejte část názvu firmy: ")
+    response_data = response.json()
 
-url_vyhledat = "https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/vyhledat"
-hlavicky = {
-    "accept": "application/json",
-    "Content-Type": "application/json"
-}
-data_vyhledani = {
-    "obchodniJmeno": nazev
-}
+    return {"subjekty": response_data["ekonomickeSubjekty"],
+            "pocet_subjektu": response_data["pocetCelkem"]}
 
-odpoved_nazev = requests.post(url_vyhledat, headers=hlavicky, json=data_vyhledani)
-data_nazev = odpoved_nazev.json()
 
-print("\nNalezeno subjektů:", data_nazev["pocetCelkem"])
-for subjekt in data_nazev["ekonomickeSubjekty"]:
-    print(f'{subjekt["obchodniJmeno"]}, {subjekt["ico"]}')
+def print_single_data(subject_data):
+    print("\nObchodní jméno:")
+    print(subject_data["obchodniJmeno"])
+    print("Sídlo:")
+    print(subject_data["sidlo"]["textovaAdresa"])
+
+def print_subjects_data(subjects_data):
+    print(f"Celkový počet: {subjects_data.get('pocet_subjektu')}")
+    for subject in subjects_data.get('subjekty'):
+        print(f'{subject["obchodniJmeno"]}, {subject["ico"]}')
+
+def search_by_ico():
+    try:
+        ico = int(input("Zadejte IČO subjektu: "))
+    except ValueError:
+        print("Nezadali jste číslo, ukončuji aplikaci")
+        exit()
+
+    subject_data = get_info_by_ico(ico)
+
+    if subject_data is None:
+        print(f"Subjekt nebyl nalezen nebo server neodpověděl správně, ukončuji aplikaci")
+        exit()
+
+    print_single_data(subject_data)
+
+def search_by_name():
+    subject_name = input("\nZadejte část názvu firmy: ")
+    subjects_data = get_info_by_name(subject_name)
+
+    if subjects_data is None:
+        print(f"Server neodpověděl správně, ukončuji aplikaci")
+        exit()
+
+    print_subjects_data(subjects_data)
+
+def main():
+    search_by_ico()
+    search_by_name()
+
+main()
+
+
+
+
